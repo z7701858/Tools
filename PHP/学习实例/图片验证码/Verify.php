2687e396ca050ad08691d11f65daa77f84d25491
Rain99 <32709921+z7701858@users.noreply.github.com>
2019-11-27 23:52:29 +0800

    图片验证码
    防重放攻击

--- PHP/学习实例/图片验证码/Verify.php
+++ PHP/学习实例/图片验证码/Verify.php
@@ -1,15 +1,20 @@
-<?php 
-if(isset($_POST['captchacode']) && $_POST['captchacode']!=null){//判断验证码输入正确与否
-	session_start();
+<?php
+session_start();
+isset($_SESSION['captchacode'])?:die("Captchacode has not been Created ! PLZ input the captchacode!");//验证session是否生成,防止在未生成时直接跳步骤以及重放攻击
 
-	if($_POST['captchacode']==$_SESSION['captchacode'])
+$captchacode=$_POST['captchacode'];
+
+if(isset($captchacode) && $captchacode!=null){//判断验证码输入正确与否
+	//session_start();
+
+	if($captchacode==$_SESSION['captchacode'])
 	{
 		echo 'Right captchacode!';
 	}
 	else
 	{
 		echo "Wrong captchacode</br>";
-		echo 'Your captchacode:'.$_POST['captchacode']."</br>";
+		echo 'Your captchacode:'.$captchacode."</br>";
 		echo 'The right captchacode:'.$_SESSION['captchacode']."</br>";
 	}
 }
@@ -17,6 +22,8 @@ else
 {
 	echo "Null captchacode is not allowed!";
 }
+
+unset($_SESSION['captchacode']);//销毁相应session
 
 echo '<p><a href="./login.html">Get back to login</a>';
  ?>
